08b860d863132ac874327d7c885b273e2959edf5
K1R1LL117 <n17k17@yandex.ru>
2025-12-29 21:43:06 +0300

    add base start app with docker

---
+++ backend_flower_shop/src/main.py
@@ -0,0 +1,28 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/ping")
+async def pong():
+    return {"msg": "pong"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        # log_level=config.app.get_log_level,
+        reload=True,
+    )
---
+++ frontend_flower_shop/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+// import axios from 'axios';
+
+function App() {
+  const api_url = "http://backend_flower_shop:8000/ping"
+
+
+
+  return (
+    <div>
+      <p>Ответ от сервера: {}</p>
+    </div>
+  )
+}
+
+export default App

a6bcab0159cb14da578e0a8607b557721087d2be
K1R1LL117 <n17k17@yandex.ru>
2026-01-25 12:33:41 +0300

    REFACTOR: move alembic to one layer with src

--- backend_flower_shop/scripts/entrypoint-backend.sh
+++ backend_flower_shop/scripts/entrypoint-backend.sh
@@ -5,7 +5,11 @@ echo "Starting backend initialization..."
 
 # Выполняем миграции
 echo "Running database migrations..."
-alembic upgrade head
+# alembic upgrade head
+# alembic -c /backend_flower_shop/alembic.ini upgrade head
+alembic -c /backend_flower_shop/alembic.ini upgrade head
+
+#  && uvicorn src.main:app --host 0.0.0.0 --port 8000
 
 # Запускаем приложение
 echo "Starting FastAPI application..."
